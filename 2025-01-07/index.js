df2448361df7ff7d7d670a989433553e4d580281
Jon Kristensen <jon@nejla.com>
2025-01-10 10:04:02 +0100

    Lägg till 2025-01-07, 2025-01-09 och 2025-01-10

---
+++ 2025-01-07/index.js
@@ -0,0 +1,11 @@
+Vue.createApp({
+  data() {
+    return {
+      image: "my-code-doesnt-work-i-have-no-idea-why-my-code-works.jpg",
+      myArray: ["hej", "hejsan", "whatever", "test"],
+      name: "Jon",
+      showErrorMessage: false,
+      typ: "A",
+    };
+  },
+}).mount("#app");
---
+++ 2025-01-09/chat/index.js
@@ -0,0 +1,37 @@
+Vue.createApp({
+  data() {
+    return {
+      messages: [
+        {
+          delivered: true,
+          reaction: "🥰",
+          self: true,
+          text: "Cảm ơn!",
+          time: "21:57",
+        },
+        {
+          delivered: true,
+          reaction: null,
+          self: false,
+          text: "Cảm ơn!",
+          time: "21:58",
+        },
+        {
+          delivered: true,
+          reaction: null,
+          self: false,
+          text: "Good night!",
+          time: "21:59",
+        },
+        {
+          delivered: false,
+          image: "cat.png",
+          reaction: "😍",
+          self: false,
+          time: "22:00",
+        },
+      ],
+      peer: "Mina",
+    };
+  },
+}).mount("#app");
