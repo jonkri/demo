a0f0b4b7411ba3721e900c1874e78f64b4555ff6
Jon Kristensen <jon@nejla.com>
2025-02-20 11:10:37 +0100

    Lägg till kod till och med 2025-02-20

---
+++ 2025-02-20/bootstrap-test/src/components/CounterButton.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from "vue";
+
+const value = ref(0);
+
+function increment() {
+  value.value++;
+}
+</script>
+
+<template>
+  <!-- <button class="btn btn-primary" @click="increment">
+    {{ value }}
+  </button> -->
+  <BButton @click="increment" variant="primary">
+    {{ value }}
+  </BButton>
+</template>
---
+++ 2025-02-20/bootstrap-test/src/components/FancyTextField.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { vBTooltip } from "bootstrap-vue-next";
+</script>
+
+<template>
+  <BButton title="Hallå där!" variant="primary" v-b-tooltip.bottom>
+    Håll muspekaren över knappen
+  </BButton>
+</template>
