4ac70738986e701fb6dc2e0c66627fda9f666098
Jon Kristensen <jon@nejla.com>
2025-02-28 10:03:39 +0100

    Lägg till kod till och med 2025-02-27

---
+++ 2025-02-27/vite-project/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import CounterButton from "./components/CounterButton.vue";
+import CounterButtonC from "./components/CounterButtonC.vue";
+import CounterButtonO from "./components/CounterButtonO.vue";
+</script>
+
+<template>
+  <CounterButton />
+  <CounterButton />
+  <CounterButton />
+  <CounterButtonC />
+  <CounterButtonC />
+  <CounterButtonC />
+  <CounterButtonO />
+  <CounterButtonO />
+  <CounterButtonO />
+</template>
