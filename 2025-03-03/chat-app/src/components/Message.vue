d70e62120ab223bd33bb203d758be170a98abef5
Jon Kristensen <jon@nejla.com>
2025-03-03 10:28:34 +0100

    Lägg till kod till och med 2025-03-03

---
+++ 2025-03-03/chat-app/src/components/Message.vue
@@ -0,0 +1,32 @@
+<template>
+  <li
+    @click="like(message)"
+    :style="{ alignSelf: message.self ? 'end' : 'start' }"
+  >
+    {{ message.text }}
+    <img
+      :alt="message.altText"
+      :src="message.image"
+      v-show="message.image !== undefined"
+    />
+    <span class="reaction">{{ message.reaction }}</span>
+    <span class="time">
+      <span v-if="message.delivered">✓</span>
+      <span v-else>⨯</span>
+    </span>
+  </li>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  message: Object,
+});
+
+// this
+// let that
+// let theThird
+</script>
+
+<style></style>
---
+++ 2025-03-03/vite-project/src/components/CounterButton.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import { ref } from "vue";
+
+  const value = ref(0);
+
+  function increment() {
+    value.value++;
+  }
+</script>
+
+<template>
+  <input @click="increment" type="button" :value="value" />
+</template>
